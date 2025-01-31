c3ba3e848aab65ce6123fe182a674760f150f462
Heliton Martins <helitonmrf@gmail.com>
2025-01-31 16:36:55 -0300

    fix: try to fix images loading

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
-  },
+      unoptimized: true,
+  }
 };
 
 module.exports = nextConfig;
