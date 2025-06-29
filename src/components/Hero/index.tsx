94fad7764e896a9ef6318c68619e086dface3ff2
Heliton Martins <helitonmrf@gmail.com>
2025-06-29 05:02:39 -0300

    wip: content

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,9 +12,10 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-O poder da Inteligência Artificial no seu negócio!                </h1>
+                  Soluções digitais inteligentes para o seu negócio
+                </h1>
                 <p className="mb-12 text-base leading-relaxed! text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  A Coral Digital é uma empresa brasileira de tecnologia que desenvolve soluções de Inteligência Artificial para o seu negócio. Ajudamos empresas a automatizar processos, reduzir custos e aumentar a eficiência.
+                  A Coral Digital é especializada em democratizar o acesso à tecnologia, oferecendo soluções digitais profissionais, personalizadas e acessíveis para impulsionar negócios locais e pequenos empreendedores.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
